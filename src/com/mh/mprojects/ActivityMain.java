57803870a8a3853893af46575fa7f9adb2996b65
Mykola <ok.mykola.13@gmail.com>
2014-12-07 20:19:16 +0200

    Init commit

---
+++ src/com/mh/mprojects/ActivityMain.java
@@ -0,0 +1,15 @@
+package com.mh.mprojects;
+
+import com.example.mprojects.R;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class ActivityMain extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
